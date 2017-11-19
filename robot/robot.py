a68113918bee4f76520c0dcb0aa5d322a17cfd6d
Andrew Barrett-Sprot <abarrettsprot@gmail.com>
2017-11-19 15:53:23 +0000

    Call `del` on the boards when cleaning them

--- robot/robot.py
+++ robot/robot.py
@@ -178,9 +178,10 @@ class Robot:
         # stop the polling threads
         for camera in self.known_cameras:
             camera._stop_poll()
+        # remove all the boards
         for board_type in self.all_known_boards:
             for board in board_type:
-                board._clean_up()
+                del board
 
     def __del__(self):
         self.close()
