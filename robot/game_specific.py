396f38b5e9616c647224a437f7a96cc0953ff1a8
Andrew Barrett-Sprot <abarrettsprot@gmail.com>
2017-12-12 11:51:33 +0000

    change marker names for SB2018

--- robot/game_specific.py
+++ robot/game_specific.py
@@ -1,16 +1,47 @@
-# ***************************************************************************
-#   NOTICE: IF YOU CHANGE THIS FILE PLEASE CHANGE ITS COUNTERPART IN ROBOTD
-# ****************************************************************************
+# ******************************************************************************
+#   NOTICE: IF YOU CHANGE THIS FILE PLEASE CHANGE ITS COUNTERPART IN SB_VISION
+# ******************************************************************************
 # Try to put all game specific code in here
 
 WALL = set(range(0, 28))  # 0 - 27
 
-# 'Tin Can Rally'
-CENTRAL_RESERVATION = set(range(28, 40))
-TOKEN = set(range(100, 200))
+# Game name TBA`
+COLUMN_N = set(range(28, 32))
+COLUMN_E = set(range(32, 36))
+COLUMN_S = set(range(36, 40))
+COLUMN_W = set(range(40, 44))
+COLUMN_FACING_N = set(range(28, 44, 4))
+COLUMN_FACING_E = set(range(29, 44, 4))
+COLUMN_FACING_S = set(range(30, 44, 4))
+COLUMN_FACING_W = set(range(31, 44, 4))
+
+# Individual Column faces.
+COLUMN_N_FACING_N = (COLUMN_N | COLUMN_FACING_N)
+COLUMN_N_FACING_S = (COLUMN_N | COLUMN_FACING_S)
+COLUMN_N_FACING_E = (COLUMN_N | COLUMN_FACING_E)
+COLUMN_N_FACING_W = (COLUMN_N | COLUMN_FACING_W)
+
+COLUMN_S_FACING_N = (COLUMN_S | COLUMN_FACING_N)
+COLUMN_S_FACING_S = (COLUMN_S | COLUMN_FACING_S)
+COLUMN_S_FACING_E = (COLUMN_S | COLUMN_FACING_E)
+COLUMN_S_FACING_W = (COLUMN_S | COLUMN_FACING_W)
+
+COLUMN_E_FACING_N = (COLUMN_E | COLUMN_FACING_N)
+COLUMN_E_FACING_S = (COLUMN_E | COLUMN_FACING_S)
+COLUMN_E_FACING_E = (COLUMN_E | COLUMN_FACING_E)
+COLUMN_E_FACING_W = (COLUMN_E | COLUMN_FACING_W)
+
+COLUMN_W_FACING_N = (COLUMN_W | COLUMN_FACING_N)
+COLUMN_W_FACING_S = (COLUMN_W | COLUMN_FACING_S)
+COLUMN_W_FACING_E = (COLUMN_W | COLUMN_FACING_E)
+COLUMN_W_FACING_W = (COLUMN_W | COLUMN_FACING_W)
+
+COLUMN = (COLUMN_N | COLUMN_E | COLUMN_S | COLUMN_W)
+
+TOKEN = set(range(44, 84))
 
 # The following constants are used to define the marker sizes
 
 MARKER_SIZES = {}
-MARKER_SIZES.update({m: (0.25, 0.25) for m in (WALL | CENTRAL_RESERVATION)})
-MARKER_SIZES.update({m: (0.8, 0.8) for m in TOKEN})
+MARKER_SIZES.update({m: (0.25, 0.25) for m in (WALL | COLUMN)})
+MARKER_SIZES.update({m: (0.1, 0.1) for m in TOKEN})
