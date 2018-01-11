c9ef126ed45e775c91022a213c7bb77882f6415d
Peter Law <PeterJCLaw@gmail.com>
2018-01-11 21:41:15 +0000

    Explicitly specify package exports

--- robot/__init__.py
+++ robot/__init__.py
@@ -1,7 +1,81 @@
 """Userspace API for a robot running ``robotd``."""
 
-from robot.robot import Robot  # noqa
-from robot.motor import BRAKE, COAST  # noqa
-from robot.game import GameMode  # noqa
-from robot.game_specific import *  # noqa
-from robot.servo import PinMode, PinValue  # noqa
+from robot.game import GameMode
+from robot.game_specific import (
+    COLUMN,
+    COLUMN_E,
+    COLUMN_E_FACING_E,
+    COLUMN_E_FACING_N,
+    COLUMN_E_FACING_S,
+    COLUMN_E_FACING_W,
+    COLUMN_FACING_E,
+    COLUMN_FACING_N,
+    COLUMN_FACING_S,
+    COLUMN_FACING_W,
+    COLUMN_N,
+    COLUMN_N_FACING_E,
+    COLUMN_N_FACING_N,
+    COLUMN_N_FACING_S,
+    COLUMN_N_FACING_W,
+    COLUMN_S,
+    COLUMN_S_FACING_E,
+    COLUMN_S_FACING_N,
+    COLUMN_S_FACING_S,
+    COLUMN_S_FACING_W,
+    COLUMN_W,
+    COLUMN_W_FACING_E,
+    COLUMN_W_FACING_N,
+    COLUMN_W_FACING_S,
+    COLUMN_W_FACING_W,
+    MARKER_SIZES,
+    TOKEN,
+    TOKEN_ZONE_0,
+    TOKEN_ZONE_1,
+    TOKEN_ZONE_2,
+    TOKEN_ZONE_3,
+    WALL,
+)
+from robot.motor import BRAKE, COAST
+from robot.robot import Robot
+from robot.servo import PinMode, PinValue
+
+__all__ = (
+    'GameMode',
+    'COLUMN',
+    'COLUMN_E',
+    'COLUMN_E_FACING_E',
+    'COLUMN_E_FACING_N',
+    'COLUMN_E_FACING_S',
+    'COLUMN_E_FACING_W',
+    'COLUMN_FACING_E',
+    'COLUMN_FACING_N',
+    'COLUMN_FACING_S',
+    'COLUMN_FACING_W',
+    'COLUMN_N',
+    'COLUMN_N_FACING_E',
+    'COLUMN_N_FACING_N',
+    'COLUMN_N_FACING_S',
+    'COLUMN_N_FACING_W',
+    'COLUMN_S',
+    'COLUMN_S_FACING_E',
+    'COLUMN_S_FACING_N',
+    'COLUMN_S_FACING_S',
+    'COLUMN_S_FACING_W',
+    'COLUMN_W',
+    'COLUMN_W_FACING_E',
+    'COLUMN_W_FACING_N',
+    'COLUMN_W_FACING_S',
+    'COLUMN_W_FACING_W',
+    'MARKER_SIZES',
+    'TOKEN',
+    'TOKEN_ZONE_0',
+    'TOKEN_ZONE_1',
+    'TOKEN_ZONE_2',
+    'TOKEN_ZONE_3',
+    'WALL',
+    'BRAKE',
+    'COAST',
+    'Robot',
+    'PinMode',
+    'PinValue',
+)
