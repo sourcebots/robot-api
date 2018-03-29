d4e364eaf6e50438e9ab2bb2b53e05063b9ee71f
Andrew Barrett-Sprot <abarrettsprot@gmail.com>
2018-03-29 23:53:43 +0100

    remove redundant str call in board

--- robot/board.py
+++ robot/board.py
@@ -156,7 +156,7 @@ class Board:
         self.socket.detach()
 
     def __repr__(self):
-        return "<{}>".format(str(self))
+        return "<{}>".format(self)
 
     def __str__(self):
         return "{} - {}".format(self.__class__.__name__, self.serial)
